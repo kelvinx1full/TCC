11c5168ad379ed18d051984c2876ce0d033971d9
Kelvin <kelvinnascimento1307@gmail.com>
2021-02-22 21:39:10 -0300

    Desenvolvimento inicial

---
+++ SistemaDeCadastros/Models/VendaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCC_VISUAL_1._0.Models
+{
+    public class VendaModel
+    {
+        public int VendaId { get; set; }
+
+    }
+}
